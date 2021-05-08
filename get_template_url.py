2ac44ab0f8e8bcab415aa4e83e7c8fc5564e0e30
Kohtaro Tanaka <kohtarotanaka@KohtaronoMacBook-Pro.local>
2021-05-09 00:07:13 +0900

    0509

--- get_template_url.py
+++ get_template_url.py
@@ -1,5 +1,15 @@
 import glob
 
 template_url = "dataset/templates/img/*"
-files = glob.glob(template_url)
-print(len(files))
+img_files = glob.glob(template_url)
+memes_path = "dataset/memes/*"
+meme_files = glob.glob(memes_path)
+img_names = []
+meme_names = []
+for i in range (0, len(meme_files)):
+    img_names.append(img_files[i].replace(".jpg","").replace("dataset/templates/img/", ""))
+    meme_names.append(meme_files[i].replace(".json","").replace("dataset/memes/", ""))
+
+for i in range (0, len(meme_files)):
+    if img_names[i] not in meme_names:
+        print(img_names[i])
